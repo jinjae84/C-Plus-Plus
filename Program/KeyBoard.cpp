c4b599b4791d9501a48dab22e896e95e8fe5b797
jinjae84 <jinjae84@naver.com>
2024-10-28 21:45:08 +0900

    (2024.10.28) 연산자 오버로딩

---
+++ Program/KeyBoard.cpp
@@ -0,0 +1,16 @@
+#include "KeyBoard.h"
+
+ KeyBoard ::KeyBoard()
+ {
+ 	cout << "Creat Keyboard" << endl;
+ }
+
+void KeyBoard::OnEnter()
+{
+	cout << "Enter Keyboard" << endl;
+}
+
+ KeyBoard::~KeyBoard()
+ {
+ 	cout << "Release Keyboard" << endl;
+ }
---
+++ Program/Mouse.cpp
@@ -0,0 +1,16 @@
+#include "Mouse.h"
+
+ Mouse::Mouse()
+ {
+ 	cout << "Creat Mouse" << endl;
+ }
+
+void Mouse::OnEnter()
+{
+	cout << "Enter Mouse" << endl;
+}
+
+ Mouse::~Mouse()
+ {
+ 	cout << "Release Mouse" << endl;
+ }
